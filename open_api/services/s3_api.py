a2f07ba1587bd8adc0a7e871cae075367de5b1a3
Badsnus <anton.098.bal@gmail.com>
2024-06-13 15:06:29 +0500

    refactor S3Api

--- open_api/services/s3_api.py
+++ open_api/services/s3_api.py
@@ -16,7 +16,7 @@ class S3Api:
         return uuid + '.' + file_extension
 
     async def create(self, db: DB, mem: Mem, file: UploadFile) -> None:
-        # TODO check status code
+        # TODO check status code - просто нужно понимать, что делать, если лег s3_service
         async with aiohttp.ClientSession() as session:
             resp = await session.post(self.upload_url, data=dict(
                 file=await file.read(),
@@ -30,17 +30,10 @@ class S3Api:
                      mem: Mem,
                      file: UploadFile,
                      old_file_extension: str) -> None:
-        # TODO check status code
-        async with aiohttp.ClientSession() as session:
-            resp = await session.post(self.upload_url, data=dict(
-                file=await file.read(),
-                key=self.get_key(mem.uuid, mem.file_extension),
-            ))
-            json = await resp.json()
-            await db.mem.update(mem, url=json['url'])
+        await self.create(db, mem, file)
 
-            if old_file_extension != mem.file_extension:
-                await self.delete(mem, old_file_extension)
+        if old_file_extension != mem.file_extension:
+            await self.delete(mem, old_file_extension)
 
     async def delete(self, mem: Mem, old_file_extension: str = None) -> None:
         async with aiohttp.ClientSession() as session:
